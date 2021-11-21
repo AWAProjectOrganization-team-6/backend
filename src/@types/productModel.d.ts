8c66fb22c905a001f42164010eb51e1f766bd9c3
Astru43 <45196326+Astru43@users.noreply.github.com>
2021-11-21 23:12:23 +0200

    Added initial models for products and special offers

---
+++ src/@types/productModel.d.ts
@@ -0,0 +1,26 @@
+import { response } from './response';
+
+type product = {
+    product_id: number;
+    restaurant_id: number;
+    name: string;
+    description: string;
+    price: number;
+    type: string;
+    picture: string;
+};
+
+type createProduct = Omit<product, 'product_id'>;
+type modifyProduct = Omit<product, 'product_id' | 'restaurant_id'>;
+
+type specialOffer = {
+    special_offer_id: number;
+    product_id: number;
+    percent_off: number;
+};
+
+type createSpecialOffer = Omit<specialOffer, 'special_offer_id'>;
+type modifySpecialOffer = Omit<specialOffer, 'special_offer_id' | 'product_id'>;
+
+type productReponse = response<product>;
+type specialOfferResponse = response<specialOffer>;
