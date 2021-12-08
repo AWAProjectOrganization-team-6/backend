4c834256262ac858aa7c51c7f771c5ff492d913a
Astru43 <45196326+Astru43@users.noreply.github.com>
2021-12-08 20:32:48 +0200

    Added env database connection

--- src/datebase.js
+++ src/datebase.js
@@ -1,8 +1,14 @@
 import postgres from 'postgres';
 
-export const sql = postgres({
-    host: 'localhost',
-    database: 'awaproject',
-    username: 'postgres',
-    password: 'postgres',
-});
+let _sql;
+
+if (process.env.DATABASE_URL) _sql = postgres(process.env.DATABASE_URL);
+else
+    _sql = postgres({
+        host: 'localhost',
+        database: 'awaproject',
+        username: 'postgres',
+        password: 'postgres',
+    });
+
+export const sql = _sql;
